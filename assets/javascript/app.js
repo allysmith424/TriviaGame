f2de5e04f9615892864bc54f45ef845a72670d15
Ally Smith <allysmith@Allys-MacBook-Air.local>
2018-01-26 09:01:23 -0600

    initial game logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,110 @@
+
+// array of questions
+
+	// label
+
+	// question
+
+	// correct answer
+
+	// incorrect answer array
+
+	// picture
+
+// variables
+
+	// used questions array
+
+	// time
+
+	// question count
+
+	// correct answers
+
+	// incorrect answers
+
+	// unanswered questions
+
+	// game complete (T/F)
+
+// function choose question ()
+
+	// set timer to 30 seconds
+
+		// change timer variable
+
+		// update DOM
+
+	// start timer ()
+
+	// if no click before time ends
+
+		// display time's up on DOM
+
+		// unasnwered ++
+
+	// choose question from array
+
+		// update DOM with object data
+
+	// push object to used questions array
+
+// function game complete ()
+
+	// game complete = true
+
+	// display final scores
+
+	// display reset button
+
+		// if clicked
+
+			// initialise game ()
+
+// function initialise game ()
+
+	// push ( / combine ) used questions array to ( / with ) main questions array
+
+	// game complete = false
+
+	// return variables to 0
+
+// listen to click
+
+	// display relevant picture
+
+	// if correct
+
+		// display "your're correct"
+
+		// correct answers  ++
+
+	// if not correct
+
+		// display "not correct"
+
+		// incorrect answers ++
+
+	// check if quesiton count
+
+		// if question count = questionsArray.length
+
+		// game complete ()
+
+	// choose question
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
